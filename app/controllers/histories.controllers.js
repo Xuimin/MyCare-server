5da5d40503cf78f542bdbf9e6fa96b60cd3335e0
Xuimin <xuimin.kt27@gmail.com>
2022-09-08 11:49:11 +0800

    Locale timezone

--- app/controllers/histories.controllers.js
+++ app/controllers/histories.controllers.js
@@ -12,7 +12,7 @@ exports.checkin = async (req, res) => {
   const user = await Users.findOne({ isActive: true })
 
   // set current date and time
-  let currentDateTime = new Date().toLocaleString();
+  let currentDateTime = new Date().getTimezoneOffset();
   
   let date = `${currentDateTime.getDate()}/${currentDateTime.getMonth() + 1}/${currentDateTime.getFullYear()}`
 
